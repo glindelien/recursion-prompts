87760c3aaf2bc5431e2d8026c47e62196d4160c3
Grace Lindelien <gracelindelien@gmail.com>
2020-06-04 18:42:15 -0700

    Write palindrome function

--- src/recursion.js
+++ src/recursion.js
@@ -181,6 +181,21 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+
+    string = string.toLowerCase();
+    string = string.split(' ').join('');
+
+    if (string.length < 2) {
+        return true;
+    }
+
+    if (string[0] === string[string.length - 1]) {
+        var restOfString = string.slice(1, string.length - 1);
+        return palindrome(restOfString);
+    } else {
+        return false;
+    }
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
